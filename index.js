3abccf50208f724a75a80547f43bea28fc60350b
ftdewgshuijk <tsjchalesmarini@hotmail.com>
2024-05-06 19:53:09 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const intersection = (a, b) => {
-  const s = new Set(b);
-  return a.filter((x) => s.has(x));
-};
+function rotateRight(head, k) {
+  if (!head || k === 0) return head;
+  let length = 1;
+  let tail = head;
+  while (tail.next) {
+    length++;
+    tail = tail.next;
+  }
+  k = k % length;
+  if (k === 0) return head;
+  let newTail = head;
+  for (let i = 1; i < length - k; i++) newTail = newTail.next;
+  const newHead = newTail.next;
+  newTail.next = null;
+  tail.next = head;
+  return newHead;
+}
