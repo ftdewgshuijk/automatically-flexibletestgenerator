f38a9cfce3dbb4ba431c82592888149f95752a31
ftdewgshuijk <tsjchalesmarini@hotmail.com>
2024-05-11 17:17:45 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
-function solveSudoku(board) {
-  solve(board);
-  function solve(board) {
-    for (let i = 0; i < 9; i++) {
-      for (let j = 0; j < 9; j++) {
-        if (board[i][j] === ".") {
-          for (let num = 1; num <= 9; num++) {
-            const numChar = num.toString();
-            if (isValid(board, i, j, numChar)) {
-              board[i][j] = numChar;
-              if (solve(board)) return true;
-              board[i][j] = ".";
-            }
-          }
-          return false;
-        }
+function threeSum(nums) {
+  nums.sort((a, b) => a - b);
+  const result = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+      let low = i + 1;
+      let high = nums.length - 1;
+      let sum = 0 - nums[i];
+      while (low < high) {
+        if (nums[low] + nums[high] === sum) {
+          result.push([nums[i], nums[low], nums[high]]);
+          while (low < high && nums[low] === nums[low + 1]) low++;
+          while (low < high && nums[high] === nums[high - 1]) high--;
+          low++;
+          high--;
+        } else if (nums[low] + nums[high] < sum) low++;
+        else high--;
       }
     }
-    return true;
   }
-  function isValid(board, row, col, num) {
-    const boxRow = Math.floor(row / 3) * 3;
-    const boxCol = Math.floor(col / 3) * 3;
-    for (let i = 0; i < 9; i++) {
-      if (
-        board[row][i] === num ||
-        board[i][col] === num ||
-        board[boxRow + Math.floor(i / 3)][boxCol + (i % 3)] === num
-      )
-        return false;
-    }
-    return true;
-  }
+  return result;
 }
