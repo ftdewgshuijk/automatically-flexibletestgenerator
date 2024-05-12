95067c33c1d4a1e61f21bbb30e5e8c57255f3509
ftdewgshuijk <tsjchalesmarini@hotmail.com>
2024-05-12 08:38:53 +0700

    demo project

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-function threeSum(nums) {
-  nums.sort((a, b) => a - b);
-  const result = [];
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
-      let low = i + 1;
-      let high = nums.length - 1;
-      let sum = 0 - nums[i];
-      while (low < high) {
-        if (nums[low] + nums[high] === sum) {
-          result.push([nums[i], nums[low], nums[high]]);
-          while (low < high && nums[low] === nums[low + 1]) low++;
-          while (low < high && nums[high] === nums[high - 1]) high--;
-          low++;
-          high--;
-        } else if (nums[low] + nums[high] < sum) low++;
-        else high--;
+function canPartitionKSubsets(nums, k) {
+  const sum = nums.reduce((acc, val) => acc + val, 0);
+  if (sum % k !== 0) return false;
+  const target = sum / k;
+  nums.sort((a, b) => b - a);
+  if (nums[0] > target) return false;
+  const visited = new Array(nums.length).fill(false);
+  return backtrack(0, 0, k);
+  function backtrack(start, currentSum, groups) {
+    if (groups === 1) return true;
+    if (currentSum === target) return backtrack(0, 0, groups - 1);
+    for (let i = start; i < nums.length; i++) {
+      if (!visited[i] && currentSum + nums[i] <= target) {
+        visited[i] = true;
+        if (backtrack(i + 1, currentSum + nums[i], groups)) return true;
+        visited[i] = false;
       }
     }
+    return false;
   }
-  return result;
 }
